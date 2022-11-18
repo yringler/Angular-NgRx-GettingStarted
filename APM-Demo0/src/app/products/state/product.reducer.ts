01195d1ab4091d74e31ed1703103ae6c8e33128e
Yehuda Ringler <yrappdev@gmail.com>
2022-11-17 23:49:44 -0500

    works with store

---
+++ APM-Demo0/src/app/products/state/product.reducer.ts
@@ -0,0 +1,13 @@
+import { createAction, createReducer, on } from "@ngrx/store";
+
+export const productReducer = createReducer(
+    {
+        showProductCode: true
+    },
+    on(createAction('[Product] Toggle Product Code'), state => {
+        return {
+            ...state,
+            showProductCode: !state.showProductCode
+        }
+    })
+);
